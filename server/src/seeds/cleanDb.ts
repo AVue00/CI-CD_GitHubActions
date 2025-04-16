46e3b66ced1125922cd79af90c089c28ed06eab7
AVue2 <Vuey00@gmail.com>
2025-04-15 21:34:13 -0400

    Commented out cleanDb function

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,19 +1,19 @@
-import models from '../models/index.js';
-import db from '../config/connection.js';
+// import models from '../models/index.js';
+// import db from '../config/connection.js';
 
-export default async (modelName: "Question", collectionName: string) => {
-  try {
-    if (!models[modelName].db.db){
-      return []
-    }
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+// export default async (modelName: "Question", collectionName: string) => {
+//   try {
+//     if (!models[modelName].db.db){
+//       return []
+//     }
+//     let modelExists = await models[modelName].db.db.listCollections({
+//       name: collectionName
+//     }).toArray()
 
-    if (modelExists.length) {
-      await db.dropCollection(collectionName);
-    }
-  } catch (err) {
-    throw err;
-  }
-}
+//     if (modelExists.length) {
+//       await db.dropCollection(collectionName);
+//     }
+//   } catch (err) {
+//     throw err;
+//   }
+// }
